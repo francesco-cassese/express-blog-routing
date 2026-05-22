c6d35f22ffbde94bc4da3b2d6f10994fd43ebb49
Francesco Cassese <francescocassese9@gmail.com>
2026-05-22 16:08:13 +0200

    feat: aggiunta route show con relativi controlli

--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -6,4 +6,61 @@ const index = (request, response) => {
     response.json(posts)
 }
 
-export { index }
+const show = (request, response) => {
+    const { id } = request.params;
+
+    if (!id) {
+        return response.status(404)
+            .json({
+                error: "ID mancante nella richiesta",
+                results: null
+            });
+    }
+
+    const numId = Number(id.trim());
+
+    if (isNaN(numId)) {
+        response.status(404)
+            .json({
+                error: `Valore dell'id non conforme o inesistente`,
+                results: null
+            })
+        return;
+    }
+
+    if (numId % 1 !== 0) {
+        return response.status(404)
+            .json({
+                error: `L'ID deve essere un numero intero`,
+                results: null
+            });
+    }
+
+    if (numId <= 0) {
+        response.status(404)
+            .json({
+                error: `Non esistono elementi con id minori o uguali a zero nel sistema`,
+                results: null
+            })
+        return;
+    }
+
+    const postsFound = posts.find(post => {
+        return post.id === numId;
+    })
+
+    if (!postsFound) {
+        return response.status(404)
+            .json({
+                error: `Post con ID ${numId} non trovato nel sistema`,
+                results: null
+            });
+    }
+
+    response.json({
+        error: null,
+        results: postsFound
+    });
+}
+
+export { index, show }
--- routers/postsRouters.js
+++ routers/postsRouters.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { index } from '../controllers/postsControllers.js'
+import { index, show } from '../controllers/postsControllers.js'
 
 const router = express.Router()
 
 router.get('/', index);
+
+router.get('/:id', show);
 
 export default router
